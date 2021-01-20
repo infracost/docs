a3b9eadf8bfd0f1f011be9429fb29e2a37653e23
Alistair Scott <ali.scott@gmail.com>
2021-01-20 18:48:40 +0000

    fix: cache GitHub star count

--- src/components/GitHubStarCount.js
+++ src/components/GitHubStarCount.js
@@ -3,14 +3,50 @@ import React, { useState, useEffect } from 'react';
 function GitHubStarCount() {
   const [stars, setStars] = useState(null);
 
+  const cacheKey = 'githubStarCount';
+  const cacheTtl = 300 * 1000;
+
+  function getCachedValue() {
+    const s = localStorage.getItem(cacheKey);
+    if (!s) {
+      return null;
+    }
+
+    const j = JSON.parse(s);
+
+    if (!j.value || !j.expiry || (new Date()).getTime() > j.expiry) {
+      return null;
+    }
+
+    return j.value;
+  }
+
+  function setCacheValue(value) {
+    const item = {
+      value,
+      expiry: (new Date()).getTime() + cacheTtl,
+    }
+
+    localStorage.setItem(cacheKey, JSON.stringify(item));
+  }
+
   useEffect(() => {
     const abortController = new AbortController();
 
     async function fetchStars() {
+      const stars = getCachedValue();
+      if (stars) {
+        setStars(stars);
+        return;
+      }
+
       const resp = await fetch('https://api.github.com/repos/infracost/infracost');
       const data = await resp.json();
+
       if (!abortController.signal.aborted) {
-        setStars(data.stargazers_count);
+        const stars = data.stargazers_count;
+        setStars(stars);
+        setCacheValue(stars);
       }
     }
 
