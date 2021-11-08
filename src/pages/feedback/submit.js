16abf37619d76bb8f1c6db5205d58ae829ed80a6
Alistair Scott <ali.scott@gmail.com>
2021-11-08 12:11:31 +0000

    feat: add feedback submitted page (#114)

---
+++ src/pages/feedback/submit.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import PageLayout from '../../components/PageLayout';
+
+function FeedbackSubmit() {
+  const { siteConfig } = useDocusaurusContext();
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+  const [submitError, setSubmitError] = useState(null);
+  
+  const urlParams = new URLSearchParams(window.location.search);
+  const value = urlParams.get('value');
+
+  const api = axios.create({
+    baseURL: siteConfig.customFields.infracostDashboardApiEndpoint,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  async function submitFeedback(data) {
+    setSubmitSuccess(false);
+    setSubmitError(null);
+
+    var resp;
+    try {
+      resp = await api.post(`/event`, {
+        event: 'infracost-feedback-submitted',
+        env: {
+          value,
+        }
+      });
+      setSubmitSuccess(true);
+    } catch(err) {
+      let msg = "Unknown error";
+      if (err.response && err.response.data && err.response.data.error) {
+        msg = err.response.data.error;
+      }
+      setSubmitError(msg);
+    }
+  }
+  
+  useEffect(() => {
+    submitFeedback();
+  }, []);
+
+  return (
+    <PageLayout
+        title="Feedback"
+        description="Infracost is open source and free for the community."
+        pageClass="feedback"
+        hideCTA={true}>
+
+      <div className="default-page-header">
+        <div className="container">
+          <div className="intro">
+            {submitSuccess && (
+              <>
+                <h1 className="tagline">Thank you!</h1>
+                <p className="sub-tagline">Your feedback has been submitted.</p>
+              </>
+            )}
+            {submitError && (
+              <>
+                <h1 className="tagline">Error submitting feedback!</h1>
+                <p className="sub-tagline">{submitError}</p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+
+    </PageLayout>
+  );
+}
+
+export default FeedbackSubmit;
