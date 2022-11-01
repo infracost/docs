b46b6d2653c4ad2a5f93069c23540c908cf4c925
Hassan Hosseini <hassankhosseini@gmail.com>
2022-11-01 11:25:20 +0000

    Update Infracost open source to Infracost CI/CD

--- src/components/questions/Questions.tsx
+++ src/components/questions/Questions.tsx
@@ -36,7 +36,7 @@ const Questions = () => {
           <QuestionLi>
             <Question>Do I need to purchase a plan now?</Question>
             <Answer>
-              No. You can start with Infracost Open Source, start seeing cloud
+              No. You can start with Infracost CI/CD, start seeing cloud
               cost estimates for infrastructure changes in CI/CD within a few
               minutes, and then upgrade to using Infracost Cloud when ready. You
               also get a free trial when you start using Infracost Cloud.
@@ -57,7 +57,7 @@ const Questions = () => {
             <Question>What happens at the end of my trial?</Question>
             <Answer>
               You can purchase a plan that is right for you and use Infracost
-              Cloud, or keep using Infracost Open Source with no disruption for
+              Cloud, or keep using Infracost CI/CD with no disruption for
               free.
             </Answer>
           </QuestionLi>
@@ -68,7 +68,7 @@ const Questions = () => {
             <Question>Do you offer support for any of the products?</Question>
             <Answer>
               Yes. We offer community support for Infracost VS Code and
-              Infracost Open Source (join our community Slack <a href="https://infracost.io/community-chat">here</a>). We also
+              Infracost CI/CD (join our community Slack <a href="https://infracost.io/community-chat">here</a>). We also
               offer paid support for all products with SLAs, please reach out to
               us on hello@infracost.io for pricing details.
             </Answer>
@@ -77,14 +77,14 @@ const Questions = () => {
           <QuestionLi>
             <Question>Do you provide onboarding / POC support?</Question>
             <Answer>
-              Yes. We can help you get up and running, and run your POC. Email us at <a href="mailto:hello@infracost.io">hello@infracost.io</a> to discuss your customer requirements. 
+              Yes. We can help you get up and running, and run your POC. Email us at <a href="mailto:hello@infracost.io">hello@infracost.io</a> to discuss your customer requirements.
             </Answer>
           </QuestionLi>
 
           <QuestionLi>
             <Question>Do you offer self-hosting options?</Question>
             <Answer>
-              You can self-host Infracost Open Source, but Infracost Cloud is
+              You can self-host Infracost CI/CD, but Infracost Cloud is
               currently only available through a SaaS subscription.
             </Answer>
           </QuestionLi>
@@ -94,8 +94,7 @@ const Questions = () => {
             <Answer>
               Infracost’s software is trusted by thousands of companies around
               the world, including many of the Fortune 500. We are open source,
-              and you can see exactly what information is sent between our
-              services. We are currently going through our SOC2 Type II
+              and you can see exactly what information is sent between your services and Infracost Cloud. We are currently going through our SOC2 Type II
               certification. We are happy to complete your company’s security
               questionnaire as part of an enterprise onboarding / POC plan.
             </Answer>
