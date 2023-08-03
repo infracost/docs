11103c00a7bd2bfa10f6da52efe57f08978febc7
Artur Janas <artur.abato.janas@gmail.com>
2023-08-03 18:14:36 +0100

    feat: add finops demo form (#513)
    * feat: add mock form section with on click scroll behaviour
    
    * feat: add form
    
    * fix: docusaurus.config infracostDashboardApiEndpoint
    
    * feat: add fetch
    
    * fixup! feat: add form
    
    * design: add error message styling
    
    * design: add success message styling
    
    * feat: add endpoint
    
    * fixup! feat: add form
    
    * chore: add inputs placeholders
    
    * feat: Add dotenv to manage env vars
    
    * enhance: Add authentication for Finops demo request
    
    * fix: Fix autoprefixer warning
    
    npm run build -> autoprefixer: end value has mixed support, consider using flex-end instead
    
    * chore: Add .env to .gitignore
    
    This file should be created manually.
    
    * chore: Fix formatting for finops.css
    
    ---------
    
    Co-authored-by: Vadim Golub <vadim@infracost.io>

--- src/components/FinopsSection/index.tsx
+++ src/components/FinopsSection/index.tsx
@@ -4,6 +4,7 @@ import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Hero from "./Hero";
 import FinopsItem from "./FinopsItem/FinopsItem";
 import LiWithCheck from "../LiWithCheck";
+import ContactForm from "./ContactForm";
 
 const url = (name: string, wrap = false) =>
   `${wrap ? "url(" : ""}/img/finops/${name}${wrap ? ")" : ""}`;
@@ -72,6 +73,7 @@ const Index = () => {
         <LiWithCheck>Build your own best practices to be monitored</LiWithCheck>
       </ul>
     </FinopsItem>,
+    <ContactForm />,
   ];
 
   const params = [
@@ -79,50 +81,67 @@ const Index = () => {
     { offset: 0.75, speed: 0.1 },
     { offset: 1.2, speed: 0.2 },
     { offset: 1.8, speed: 0.3 },
-    { offset: 2.1, speed: 0.2 },
+    { offset: 2.1, speed: 0.4 },
+    { offset: 2.6, speed: 0.3 },
   ];
 
-  return (
-    <div className="finops-parallax container">
-      <div id="finops-parallax">
-        <Parallax ref={parallax} pages={3}>
-          {/* main background */}
-          <ParallaxLayer
-            offset={0}
-            speed={0}
-            factor={3}
-            style={{
-              backgroundImage: url("stars.svg", true),
-              backgroundSize: "cover",
-            }}
-          />
+  const handleDemoButtonClick = () => {
+    const anchor = document.querySelector("#finops-mobile #finops-form");
+    if (anchor) {
+      anchor.scrollIntoView();
+    }
+    parallax.current.scrollTo(3.5);
+  };
 
-          {items.map((item, index) => (
+  return (
+    <>
+      <div className="finops-cta container">
+        <div className="finops-cta__wrapper">
+          <a className="button primary" onClick={handleDemoButtonClick}>
+            Request live demo now
+          </a>
+        </div>
+      </div>
+      <div className="finops-parallax container">
+        <div id="finops-parallax">
+          <Parallax ref={parallax} pages={3.5}>
             <ParallaxLayer
-              offset={params[index].offset}
-              speed={params[index].speed}
-              key={index}
+              offset={0}
+              speed={0}
+              factor={3}
               style={{
-                padding: "15px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                zIndex: `${index} + 1`,
+                backgroundImage: url("stars.svg", true),
+                backgroundSize: "cover",
               }}
-            >
+            />
+
+            {items.map((item, index) => (
+              <ParallaxLayer
+                offset={params[index].offset}
+                speed={params[index].speed}
+                key={index}
+                style={{
+                  padding: "15px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  zIndex: `${index} + 1`,
+                }}
+              >
+                {item}
+              </ParallaxLayer>
+            ))}
+          </Parallax>
+        </div>
+        <div id="finops-mobile">
+          {items.map((item, index) => (
+            <div key={index} className="finops-mobile-item">
               {item}
-            </ParallaxLayer>
+            </div>
           ))}
-        </Parallax>
+        </div>
       </div>
-      <div id="finops-mobile">
-        {items.map((item, index) => (
-          <div key={index} className="finops-mobile-item">
-            {item}
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
