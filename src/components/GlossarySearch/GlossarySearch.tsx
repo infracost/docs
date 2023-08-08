e97acee2cb56339038d98bff2a4b48d9c3c6a2eb
Artur Janas <artur@infracost.io>
2023-08-08 11:42:08 +0100

    feat: add glossaryTerms.json

--- src/components/GlossarySearch/GlossarySearch.tsx
+++ src/components/GlossarySearch/GlossarySearch.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import terms from "./glossaryTerms.json";
 
 const GlossarySearch = () => {
-  return <div className="container">Search FinOps Glossary</div>;
+
+	
+  return (
+    <div className="container">
+      Search FinOps Glossary
+      {terms.map((term) => (
+        <li key={term.key}>
+          {term.key}: {term.description}
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default GlossarySearch;
