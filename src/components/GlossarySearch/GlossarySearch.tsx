479bcb3217186ceeb4fb73fde7f3fcba3abce6b8
Artur Janas <artur@infracost.io>
2023-08-08 13:56:34 +0100

    feat: add page header and search input styling

--- src/components/GlossarySearch/GlossarySearch.tsx
+++ src/components/GlossarySearch/GlossarySearch.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import terms from "./glossaryTerms.json";
+import "./GlossarySearch.css";
+import SearchIcon from "../icons/SearchIcon";
 
 const GlossarySearch = () => {
-
-	
   return (
-    <div className="container">
-      Search FinOps Glossary
-      {terms.map((term) => (
-        <li key={term.key}>
-          {term.key}: {term.description}
-        </li>
-      ))}
-    </div>
+    <>
+      <div className="intro ">
+        <div className="container glossary-search__intro-container">
+          <h1 className="tagline">FinOps Glossary</h1>
+          <div className="finops-glossary__search">
+            <input
+              type="search"
+              placeholder="Search FinOps Glossary"
+              className="glossary-search__input"
+            />
+            <SearchIcon />
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        {terms.map((term) => (
+          <li key={term.key}>
+            {term.key}: {term.description}
+          </li>
+        ))}
+      </div>
+    </>
   );
 };
 
