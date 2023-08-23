aa0feb31c73349e29e03d2d9991cc49efa6e8e5e
Artur Janas <artur@infracost.io>
2023-08-23 11:36:24 +0100

    chore: add images

--- src/components/GlossaryTermPage/GlossaryTermPageAside.tsx
+++ src/components/GlossaryTermPage/GlossaryTermPageAside.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { GlossaryTermType } from '../GlossarySearch';
+import OpenBookIcon from '../icons/OpenBookIcon';
+
+const imgUrlPrefix = '/img/glossary/';
 
 const GlossaryTermPageAside = ({ term }: { term: GlossaryTermType }) => (
   <aside className="glossary-term__aside">
-    {term.imgUrl && (
-      <div>
-        <img className="glossary-term__aside--img" src={term.imgUrl} alt="" />
+    <div className="glossary-term__aside-img-container">
+      <img
+        className="glossary-term__aside-img"
+        src={term.imgUrl ? `${imgUrlPrefix}/${term.imgUrl}` : `${imgUrlPrefix}/generic.png`}
+        alt=""
+      />
+      <div className="glossary-term__aside-def">
+        <OpenBookIcon />
+        <span>Definition</span>
       </div>
-    )}
+    </div>
     <div className="glossary-term__aside-content">
-      <h3 className="glossary-term__header">
-        {term.termTitle}
-        <span className="glossary-term__def">(definition)</span>
-      </h3>
+      <h3 className="glossary-term__header">{term.termTitle}</h3>
       <span className="glossary-term__definition">{term.definition}</span>
     </div>
   </aside>
