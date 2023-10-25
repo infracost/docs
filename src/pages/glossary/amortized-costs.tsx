a1b9185bf3d77941c90affae7faad4f547ef62f3
Alistair Scott <ali.scott@gmail.com>
2023-10-25 15:20:54 +0100

    chore: remove old content

--- src/pages/glossary/amortized-costs.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import GlossaryPage from '../../components/GlossaryPage';
-
-const Index = () => <GlossaryPage slug="amortized-costs" />;
-
-export default Index;
--- src/pages/glossary/ci-cd.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import GlossaryPage from '../../components/GlossaryPage';
-
-const Index = () => <GlossaryPage slug="ci-cd" />;
-
-export default Index;
