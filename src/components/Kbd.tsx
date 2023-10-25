8b2f06244e3a1a64e8456124247ecc69a45203a9
Alistair Scott <ali.scott@gmail.com>
2023-10-25 15:20:54 +0100

    chore: remove old content

--- src/components/Kbd.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Kbd = ({ keyName }) => <kbd className="kbd">{keyName}</kbd>;
-
-export default Kbd;
--- src/pages/glossary/terraform.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import GlossaryPage from '../../components/GlossaryPage';
-
-const Index = () => <GlossaryPage slug="terraform" />;
-
-export default Index;
